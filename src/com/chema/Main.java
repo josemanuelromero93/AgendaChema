d86f1addb4cfbf08a84d7d411331ed251504fa06
Chema <josemanuelromero93@outlook.com>
2017-01-29 16:06:09 +0100

    Added Command, CommandParser and Prompt

--- src/com/chema/Main.java
+++ src/com/chema/Main.java
@@ -5,4 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+
+    class Persona {
+
+        String name;
+        String phone;
+
+    }
 }
